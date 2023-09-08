3cc86dd0838e0b83ded4cc3522a684dd9691afdc
Jhay-Great <alemonai.jenef@gmail.com>
2023-09-08 20:02:00 +0000

    removed unwanted text

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const time = document.querySelector('[data-testid="currentUTCTime"]');
 
 
 const date = new Date();
-day.textContent = `Today: ${new Intl.DateTimeFormat('en-GB', {weekday: "long"}).format(date)}`;
+day.textContent = `${new Intl.DateTimeFormat('en-GB', {weekday: "long"}).format(date)}`;
 
 const timeInMilliseconds = date.getTime();
-time.textContent = `Time: ${timeInMilliseconds}`
+time.textContent = `${timeInMilliseconds}`
